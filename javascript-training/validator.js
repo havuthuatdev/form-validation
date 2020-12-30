58ebcb2584ed6319485f628afa3f8c41acd49c2e
PN\thuat.ha21 <havuthuatdev@gmail.com>
2020-12-30 17:32:49 +0700

    validator- rules

---
+++ javascript-training/validator.js
@@ -0,0 +1,13 @@
+// hàm validator
+function Validator(options){
+    // console.log(options.form);
+    var formElement = document.querySelector(options.form);
+    // console.log(formElement)
+}
+// định nghĩa các rules
+Validator.isRequired =function(){
+
+}
+Validator.isEmail =function(){
+
+}
